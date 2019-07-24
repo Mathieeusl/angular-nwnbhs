02aa0d198413015a6c342e00e9ff2638fcf16870
Mathieeusl <mathieeu@outlook.com>
2019-07-24 11:06:40 -0400

    2 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit} from '@angular/core';
-import {Hero} from '../hero';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -7,17 +7,26 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-hero: Hero = {
-  id: 1,
-  name: 'Windstorm'
-}
-heroes = HEROES;
-  constructor() { 
-    
-    }
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
